4df6407ec7325a3ea3287b2b7a04318e6c5a2a44
hadi-naqvi <hadinaqvi04@gmail.com>
2021-11-29 19:00:26 -0500

    Added Pawn.Java & King.java classes

---
+++ King.java
@@ -0,0 +1,30 @@
+/**
+ * [ICS4U] Checkers | King.java 
+ * Date: December 2nd, 2021
+ * @author Hadi Naqvi
+ * Teacher: Mr. Ho
+ */
+
+public class King extends Piece {
+    /**
+     * Constructor method for King objects
+     * @param row The row number when the King is initialized
+     * @param col The column number when the King is initialized
+     * @param marker The marker when the King is initialized
+     */
+    public King(int row, int col, String marker) {
+         super(row, col, marker);
+    }
+
+    /**
+     * This method returns a 2D arraylist containing the coordinates of all the possible moves that the King can make
+     * @param board The 2D array which stores the pieces on the checkerboard
+     * @return The list of possible moves that can be made
+     */
+    @Override
+    public ArrayList<ArrayList<Integer>> getPossibleMoves(Piece[][] board) {
+        ArrayList<ArrayList<Integer>> possibleMoves = new ArrayList<ArrayList<Integer>>();
+
+        return possibleMoves;
+    }
+}
---
+++ Pawn.java
@@ -0,0 +1,30 @@
+/**
+ * [ICS4U] Checkers | Pawn.java 
+ * Date: December 2nd, 2021
+ * @author Hadi Naqvi
+ * Teacher: Mr. Ho
+ */
+
+public class Pawn extends Piece {
+    /**
+     * Constructor method for Pawn objects
+     * @param row The row number when the Pawn is initialized
+     * @param col The column number when the Pawn is initialized
+     * @param marker The marker when the Pawn is initialized
+     */
+    public Pawn(int row, int col, String marker) {
+         super(row, col, marker);
+    }
+
+    /**
+     * This method returns a 2D arraylist containing the coordinates of all the possible moves that the Pawn can make
+     * @param board The 2D array which stores the pieces on the checkerboard
+     * @return The list of possible moves that can be made
+     */
+    @Override
+    public ArrayList<ArrayList<Integer>> getPossibleMoves(Piece[][] board) {
+        ArrayList<ArrayList<Integer>> possibleMoves = new ArrayList<ArrayList<Integer>>();
+
+        return possibleMoves;
+    }
+}
